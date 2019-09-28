a0aa0822bc438bb3f572ebb48282ee7b04592f45
stavrdriff <49834186+stavrdriff@users.noreply.github.com>
2019-09-28 12:09:40 +0300

    Add files via upload

--- TimSolo/js/scripts.js
+++ TimSolo/js/scripts.js
@@ -7,7 +7,14 @@ $(".top").click(function () {
 
 // _________________
 
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const closeBtn = document.querySelector('.close-btn');
+const links = document.querySelectorAll('header nav ul li a');
 
+burger.addEventListener('click',e=>menu.classList.toggle('open'));
+closeBtn.addEventListener('click',e=>menu.classList.remove('open'));
+[...links].forEach(link=>link.addEventListener('click',e=>menu.classList.remove('open')));
 
 
 
